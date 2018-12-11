3aad28c344d24ef9c3a136296190c694bef4d93b
jian.yang <jian.yang@rokid.com>
2018-12-11 10:47:36 +0800

    init kotlin in action project

---
+++ kotlin-in-action/src/com/yeungeek/kotlin/Main.java
@@ -0,0 +1,11 @@
+package com.yeungeek.kotlin;
+
+import com.yeungeek.kotlin.chapter1.BasicGrammarKt;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println(BasicGrammarKt.fromKotlin());
+    }
+}
---
+++ kotlin-in-action/src/com/yeungeek/kotlin/chapter1/BasicGrammar.kt
@@ -0,0 +1,9 @@
+package com.yeungeek.kotlin.chapter1
+
+fun main() {
+    println("Hello Kotlin")
+}
+
+fun fromKotlin(): String {
+    return "From Kotlin"
+}
